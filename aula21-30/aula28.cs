1a37c1be14c3cfb0dd09603912017b0896bd285d
Fernando Sena <fernandosenacruz@gmail.com>
2020-08-13 16:41:27 -0300

    mod

--- aula21-30/aula28.cs
+++ aula21-30/aula28.cs
@@ -8,6 +8,7 @@ public class Player{ // por padrão as classes são publicas a não ser que se e
 class Aula28{
     static void Main(){
         Player p1=new Player();
+        Player p2=new Player();
 
         if(p1.hp<1){
             p1.morto="morto";
@@ -15,6 +16,16 @@ class Aula28{
         }else{
             p1.vivo="vivo";
             Console.WriteLine("Hp do player 1 é de: {0}%. Ele está {1}.",p1.hp, p1.vivo);
+        }
+
+        p2.hp=10;
+
+        if(p2.hp<1){
+            p2.morto="morto";
+            Console.WriteLine("Hp do player 2 é de: {0}%. Ele está {1}.",p2.hp, p2.morto);
+        }else{
+            p2.vivo="vivo";
+            Console.WriteLine("Hp do player 2 é de: {0}%. Ele está {1}.",p2.hp, p2.vivo);
         }              
     }
 }
