9087660b7a2813b705423cb030258c4873de2ece
Fernando Sena <fernandosenacruz@gmail.com>
2020-08-13 16:32:04 -0300

    add aula28

--- aula21-30/aula28.cs
+++ aula21-30/aula28.cs
@@ -1,7 +1,20 @@
 using System;
-// 
+// Classes e Objetos
+
+public class Player{ // por padrão as classes são publicas a não ser que se especifique.
+    public int hp=0;
+    public string vivo, morto;
+}
 class Aula28{
     static void Main(){
-        
+        Player p1=new Player();
+
+        if(p1.hp<1){
+            p1.morto="morto";
+            Console.WriteLine("Hp do player 1 é de: {0}%. Ele está {1}.",p1.hp, p1.morto);
+        }else{
+            p1.vivo="vivo";
+            Console.WriteLine("Hp do player 1 é de: {0}%. Ele está {1}.",p1.hp, p1.vivo);
+        }              
     }
 }
